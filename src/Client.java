80d20b5b67db276bd841cac3fa1a0b5916a1f3d7
JETHRO ESTANGKI <2212047@slu.edu.ph>
2022-02-08 10:27:55 +0800

    Add Client.java

---
+++ src/Client.java
@@ -0,0 +1,8 @@
+public class Client {
+
+    public static void main(String[] args) {
+        //Controller controller = new Controller();
+        View view = new View();
+        view.setVisible(true);
+    }
+}
