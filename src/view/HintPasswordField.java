60dc22c23ca0d9ece3b0bb855352578e225faf7e
JETHRO ESTANGKI <2212047@slu.edu.ph>
2022-02-09 15:44:13 +0800

    Add HintPasswordField and HintTextField

---
+++ src/view/HintPasswordField.java
@@ -0,0 +1,46 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+public class HintPasswordField extends JPasswordField implements FocusListener {
+
+    private final String hint;
+    private boolean showingHint;
+
+    public HintPasswordField(final String hint) {
+        super(hint);
+        this.setForeground(Color.GRAY);
+        super.setEchoChar((char) 0);
+        this.hint = hint;
+        this.showingHint = true;
+        super.addFocusListener(this);
+    }
+
+    @Override
+    public void focusGained(FocusEvent e) {
+        if (this.getText().isEmpty()) {
+            super.setForeground(Color.BLACK);
+            super.setEchoChar('*');
+            super.setText("");
+            showingHint = false;
+        }
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+        if (this.getText().isEmpty()) {
+            super.setForeground(Color.GRAY);
+            super.setEchoChar((char) 0);
+            super.setText(hint);
+            showingHint = true;
+        }
+    }
+
+    @Override
+    public String getText() {
+        return showingHint ? "" : super.getText();
+    }
+}
---
+++ src/view/HintTextField.java
@@ -0,0 +1,43 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+public class HintTextField extends JTextField implements FocusListener {
+
+    private final String hint;
+    private boolean showingHint;
+
+    public HintTextField(final String hint) {
+        super(hint);
+        this.setForeground(Color.GRAY);
+        this.hint = hint;
+        this.showingHint = true;
+        super.addFocusListener(this);
+    }
+
+    @Override
+    public void focusGained(FocusEvent e) {
+        if (this.getText().isEmpty()) {
+            super.setForeground(Color.BLACK);
+            super.setText("");
+            showingHint = false;
+        }
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+        if (this.getText().isEmpty()) {
+            super.setForeground(Color.GRAY);
+            super.setText(hint);
+            showingHint = true;
+        }
+    }
+
+    @Override
+    public String getText() {
+        return showingHint ? "" : super.getText();
+    }
+}
