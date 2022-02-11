a70ad36f95586c26ac5415ccb35212794cdb124f
JETHRO ESTANGKI <2212047@slu.edu.ph>
2022-02-11 14:26:13 +0800

    Add the two main classes

---
+++ src/Client.java
@@ -0,0 +1,26 @@
+import client.controller.ClientController;
+import client.controller.LogInController;
+import common.UserModel;
+import server.controller.AdminController;
+
+import java.net.Socket;
+
+public class Client {
+
+    private static LogInController logInController;
+    private static ClientController clientController;
+    private static AdminController adminController;
+    private static UserModel user;
+    private static Socket socket;
+
+    public static void main(String[] args) {
+        // start client
+
+        logInController = new LogInController();  //show the login frame
+        user = logInController.getUser();         // get the user that logged in
+
+
+        clientController = new ClientController(socket, user);
+
+    }
+}
