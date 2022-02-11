3fa92429d1230aa96622dacbd2f52666963192d5
JETHRO ESTANGKI <2212047@slu.edu.ph>
2022-02-11 10:59:30 +0800

    Rename ClientController

--- src/controller/ClientController.java
+++ src/controller/ClientController.java
@@ -1,4 +1,34 @@
 package controller;
 
-public class ClientController {
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+class ClientController implements Runnable{
+    private Socket client;
+    private BufferedReader reader;
+    private PrintWriter writer;
+
+    //list of connected clients
+    public static ArrayList<ClientController> clientHandlerControllers = new ArrayList<>();
+
+    /**
+     * Creates
+     * @param clientSocket
+     */
+    public ClientController(Socket clientSocket){
+            try {
+                this.client = clientSocket;
+                reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                writer = new PrintWriter(client.getOutputStream(),true);
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    @Override
+    public void run(){
+        //TODO what the server will do when called
+
+    }
 }
