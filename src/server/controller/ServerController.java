d7ca635a66b4c99ac3c11f57c86cd903c4189657
JETHRO ESTANGKI <2212047@slu.edu.ph>
2022-02-17 13:42:39 +0800

    Move run method of ServerController to ServerModel

--- src/server/controller/ServerController.java
+++ src/server/controller/ServerController.java
@@ -8,7 +8,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -19,62 +18,15 @@ import java.util.concurrent.Executors;
  * run the server
  * read the list of users
  * wait for validate users
- *
  */
 public class ServerController {
-    public static ArrayList<UserModel> users;
     public ServerModel serverModel;
-    private static final int port = 2022;
-    private static ServerSocket serverSocket;
-    private static Socket clientSocket;
-    private static ExecutorService pool = Executors.newCachedThreadPool();
+
 
     public ServerController() {
         serverModel = new ServerModel(Utility.readData("res/data.dat"), new ArrayList<>());
     }
 
     public void run() {
-        ArrayList<ClientController> clients = new ArrayList<>();
-        serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(port);
-            System.out.println("Server Started");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            // Accept client connection
-            clientSocket = serverSocket.accept();
-            System.out.println("Client connected: " + clientSocket);
-            pool = Executors.newCachedThreadPool();
-            ObjectOutputStream outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
-            ObjectInputStream inputStream = new ObjectInputStream(clientSocket.getInputStream());
-            //ClientController clientThread = new ClientController(clientSocket);
-            AuthenticatorModel authenticate;
-            Object input;
-            while (true) {
-                input = inputStream.readObject();
-                if (input.equals("login")) {
-                    authenticate = new AuthenticatorModel(inputStream, outputStream, serverModel.getRegisteredUsers());
-                    String username = (String) inputStream.readObject();
-                    String password = (String) inputStream.readObject();
-                    System.out.printf("Attempting to login with username:%s and password:%s\n", username, password);
-                    if (authenticate.verifyUser(username, password)) {
-                        ClientHandlerModel client = new ClientHandlerModel(clientSocket, inputStream, outputStream, serverModel.getChatHistory());
-                        //clients.add(clientThread);
-                        pool.execute(client);
-                    }
-                } else if (input.equals("register")) {
-                    System.out.println("Attempting to register.");
-                    input = inputStream.readObject();
-                    serverModel.addRegisteredUser((UserModel) input);
-                    Utility.exportData(serverModel.getRegisteredUsers());
-                    outputStream.writeObject("registered");
-                }
-            }
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
     }
 }
--- src/server/model/ServerModel.java
+++ src/server/model/ServerModel.java
@@ -14,10 +14,69 @@ import java.util.concurrent.Executors;
 public class ServerModel {
     List<UserModel> registeredUsers;
     List<MessageModel> chatHistory;
+    List<ClientController> clients;
+    private static final int port = 2022;
+    private static ServerSocket serverSocket;
+    private static Socket clientSocket;
+    private static ExecutorService pool;
 
     public ServerModel(List<UserModel> registeredUsers, List<MessageModel> chatHistory) {
         this.registeredUsers = registeredUsers;
         this.chatHistory = chatHistory;
+        pool = Executors.newFixedThreadPool(2);
+    }
+
+    public void run() {
+        clients = new ArrayList<>();
+        serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server Started");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (true) {
+            ObjectOutputStream outputStream = null;
+            ObjectInputStream inputStream = null;
+            try {
+                // Accept client connection
+                clientSocket = serverSocket.accept();
+                System.out.println("Client connected: " + clientSocket);
+                outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+                inputStream = new ObjectInputStream(clientSocket.getInputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //ClientController clientThread = new ClientController(clientSocket);
+            AuthenticatorModel authenticate;
+            Object input;
+            while (true) {
+                try {
+                    input = inputStream.readObject();
+                    if (input.equals("login")) {
+                        authenticate = new AuthenticatorModel(inputStream, outputStream, getRegisteredUsers());
+                        String username = (String) inputStream.readObject();
+                        String password = (String) inputStream.readObject();
+                        System.out.printf("Attempting to login with username:%s and password:%s\n", username, password);
+                        if (authenticate.verifyUser(username, password)) {
+                            ClientHandlerModel client = new ClientHandlerModel(clientSocket, inputStream, outputStream, getChatHistory());
+                            //clients.add(clientThread);
+                            pool.execute(client);
+                        }
+                    } else if (input.equals("register")) {
+                        System.out.println("Attempting to register.");
+                        input = inputStream.readObject();
+                        addRegisteredUser((UserModel) input);
+                        Utility.exportData(getRegisteredUsers());
+                        outputStream.writeObject("registered");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
     }
 
     private String checkStatus(String username) {
@@ -55,5 +114,6 @@ public class ServerModel {
     public void addRegisteredUser(UserModel user) {
         this.registeredUsers.add(user);
     }
+
 
 }
